25ff6324a458fce1c7f6b202661b5eaf90a755cc
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 23:22:32 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,12 @@
+[
+  "modern gym interior with equipment",
+  "group fitness class in gym",
+  "personal trainer working with client",
+  "gym exterior building",
+  "gym locker room area",
+  "gym cardio equipment section",
+  "gym weight training area",
+  "yoga class in progress",
+  "gym staff team meeting",
+  "happy gym members working out"
+]
